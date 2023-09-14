6913175247a3771f176f8af4d77d35225bf5d20d
Paula Maria Duganiuk <143445426+PaulaMaria1998@users.noreply.github.com>
2023-09-14 19:37:44 +0200

    Covid Portfolio.sql

--- Covid Portfolio.sql
+++ Covid Portfolio.sql
@@ -1,9 +1,6 @@
 /*
-Covid 19 Data Exploration 
+Covid Portfolio
 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
-
-*/
 
 Select *
 From PortfolioProject..CovidDeaths
